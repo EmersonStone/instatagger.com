9e661f784bf1db449f5c537d7fb5d0b14dde337c
Malinda Mosholder <m.m.mosholder@gmail.com>
2017-07-06 13:30:10 -0600

    scaffold marketing landing page

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -14,8 +14,32 @@
               <a href="#" class="button"></a>
             </div>
           </div>
+
+          <div class="how-it-works">
+            <h2>How it works</h2>
+            <div class="-item">
+              <div class="-title">1. First Step</div>
+              <div class="-action"></div>
+            </div>
+
+            <div class="-item">
+              <div class="-title">2. Second Step</div>
+              <div class="-action"></div>
+            </div>
+
+            <div class="-item">
+              <div class="-title">3. Third Step</div>
+              <div class="-action"></div>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div class="testimonial">
+        <div class="-quote"><em>"This is the best app I've seen on the internets."</em></div>
+        <div class="-author">-Brett</div>
+      </div>
+    
     </main>
     <site-footer></site-footer>
   </div>

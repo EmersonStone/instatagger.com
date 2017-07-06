0a20b588ae6d496962790779eff51ef3146858fe
Malinda Mosholder <m.m.mosholder@gmail.com>
2017-07-06 12:16:37 -0600

    continued layout building

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,6 @@
   <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16" /> --}}
 
-  <script src="https://use.fontawesome.com/19d2f842d5.js"></script>
-
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
